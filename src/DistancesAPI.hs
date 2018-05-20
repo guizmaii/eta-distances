10df20295993bbab7eda6d56cd69eeb1f3ec65ce
jules Ivanic <jules.ivanic@gmail.com>
2018-05-20 20:01:01 +0200

    `DistanceAPI#distance` shoudl return a `Map TravelMode Distance`

--- src/DistancesAPI.hs
+++ src/DistancesAPI.hs
@@ -1,6 +1,7 @@
 module DistancesAPI where
 
 import Prelude hiding (length)
+import qualified Data.Map.Strict as Map
 
 data LatLong    = LatLong { latitude :: Double, longitude :: Double }
   deriving Show
@@ -9,13 +10,17 @@ data Distance   = Distance { length :: Double, duration :: Double }
   deriving Show
 
 data TravelMode = Driving | Bicycling | Unknown
-  deriving Show
+  deriving (Show, Ord, Eq)
 
 -- TODO: Implement it. Dumb for now.
 distanceProvider :: LatLong -> LatLong -> TravelMode -> IO (TravelMode, Distance)
 distanceProvider _ _ travelMode = pure (travelMode, Distance { length = 3, duration = 4 })
 
-distance :: LatLong -> LatLong -> [TravelMode] -> IO [(TravelMode, Distance)]
+-- TODO: I don't understand why I need to write `Map.Map` in the type signature.
+distance :: LatLong -> LatLong -> [TravelMode] -> IO (Map.Map TravelMode Distance)
 distance origin destination travelModes =
-  let configuredDistanceProvider = distanceProvider origin destination
-  in traverse configuredDistanceProvider travelModes
+  let
+    configuredDistanceProvider = distanceProvider origin destination
+    distancesPairs = traverse configuredDistanceProvider travelModes
+  in
+    fmap Map.fromList distancesPairs
