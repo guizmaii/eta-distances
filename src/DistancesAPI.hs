e631243ab4c6b5aafb01ebccf62e904cfa250715
jules Ivanic <jules.ivanic@gmail.com>
2018-05-20 19:42:49 +0200

    Use automatic Typeclass instance derivation

--- src/DistancesAPI.hs
+++ src/DistancesAPI.hs
@@ -3,19 +3,19 @@ module DistancesAPI where
 import Prelude hiding (length)
 
 data LatLong    = LatLong { latitude :: Double, longitude :: Double }
+  deriving Show
+
 data Distance   = Distance { length :: Double, duration :: Double }
+  deriving Show
+
 data TravelMode = Driving | Bicycling | Unknown
-
-instance Show Distance where
-  show (Distance o d) = "Distance( length = " ++ show o ++ ", duration = " ++ show d ++ " )"
-
--- [TravelMode] -> (TravelMode -> LatLong -> LatLong -> IO Distance) -> [IO Distance] -> IO [Distance]
+  deriving Show
 
 -- TODO: Implement it. Dumb for now.
-distanceProvider :: LatLong -> LatLong -> TravelMode -> IO Distance
-distanceProvider _ _ _ = pure Distance { length = 3, duration = 4 }
+distanceProvider :: LatLong -> LatLong -> TravelMode -> IO (TravelMode, Distance)
+distanceProvider _ _ travelMode = pure (travelMode, Distance { length = 3, duration = 4 })
 
-distance :: LatLong -> LatLong -> [TravelMode] -> IO [Distance]
+distance :: LatLong -> LatLong -> [TravelMode] -> IO [(TravelMode, Distance)]
 distance origin destination travelModes =
   let configuredDistanceProvider = distanceProvider origin destination
   in traverse configuredDistanceProvider travelModes
