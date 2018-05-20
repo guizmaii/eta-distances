14f4ee7f338ac679d91afd3dadec1c42bc1575eb
jules Ivanic <jules.ivanic@gmail.com>
2018-05-20 21:14:12 +0200

    Improve code readability thanks to the `&` operator

--- src/DistancesAPI.hs
+++ src/DistancesAPI.hs
@@ -2,15 +2,24 @@ module DistancesAPI where
 
 import Prelude hiding (length)
 import qualified Data.Map.Strict as Map
+import Data.Function ((&))
 
-data LatLong    = LatLong { latitude :: Double, longitude :: Double }
+data LatLong = LatLong { latitude :: Double, longitude :: Double }
+  deriving (Show, Eq)
+
+data Distance = Distance { length :: Double, duration :: Double }
   deriving Show
 
-data Distance   = Distance { length :: Double, duration :: Double }
-  deriving Show
+zero :: Distance
+zero = Distance { length = 0, duration = 0 }
 
 data TravelMode = Driving | Bicycling | Unknown
   deriving (Show, Ord, Eq)
+
+data DirectedPath = DirectedPath { origin :: LatLong
+                                 , destination :: LatLong
+                                 , travelModes :: [TravelMode] }
+  deriving Show
 
 -- TODO: Implement it. Dumb for now.
 distanceProvider :: LatLong -> LatLong -> TravelMode -> IO (TravelMode, Distance)
@@ -18,9 +27,13 @@ distanceProvider _ _ travelMode = pure (travelMode, Distance { length = 3, durat
 
 -- TODO: I don't understand why I need to write `Map.Map` in the type signature.
 distance :: LatLong -> LatLong -> [TravelMode] -> IO (Map.Map TravelMode Distance)
+distance _ _ []                         = pure Map.empty
 distance origin destination travelModes =
   let
-    configuredDistanceProvider = distanceProvider origin destination
-    distancesPairs = traverse configuredDistanceProvider travelModes
+    distancesPairs =
+      if origin == destination then
+        pure (travelModes & fmap (\x -> (x, zero)))
+      else
+        travelModes & traverse (distanceProvider origin destination)
   in
-    fmap Map.fromList distancesPairs
+    distancesPairs & fmap Map.fromList
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import DistancesAPI
+import Data.Function ((&))
 
 main :: IO ()
 main = let
          origin      = LatLong { latitude = 2, longitude = 2 }
          destination = LatLong { latitude = 3, longitude = 3 }
          d           = distance origin destination [Driving, Bicycling]
-       in fmap show d >>= putStrLn
+       in
+         d & fmap show >>= putStrLn
